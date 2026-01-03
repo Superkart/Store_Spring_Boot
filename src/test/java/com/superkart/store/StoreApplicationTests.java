f6ee9b86f74219d2a297c11cb0641e3169d917d6
Superkart <karthik.ragi15@gmail.com>
2026-01-02 18:29:59 -0600

    Initial commit

---
+++ src/test/java/com/superkart/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superkart.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
